107a1f64ca5e88b5b2982c62266c1706dbffe9b8
linzexin <linzexinmasterchief@gmail.com>
2019-11-11 15:32:37 +0000

    Initial commit

---
+++ Assets/Scripts/BikeControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeControl : MonoBehaviour
+{
+
+    public float preset_flight_height;
+    private float delta_height;
+    public float max_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        preset_flight_height = transform.position.y;
+        delta_height = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 30);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(-transform.forward * 30);
+        }
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    private Vector3 delta_pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        delta_pos = target.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position - delta_pos;
+    }
+}
