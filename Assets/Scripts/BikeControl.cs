abd79b9bab7c262a33b959172d7ce1490604b295
linzexin <linzexinmasterchief@gmail.com>
2019-11-14 03:21:15 +0000

    add engine flare

--- Assets/Scripts/BikeControl.cs
+++ Assets/Scripts/BikeControl.cs
@@ -10,6 +10,8 @@ public class BikeControl : MonoBehaviour
     [SerializeField] GameObject frfp;
     // back floating point
     [SerializeField] GameObject bfp;
+    // main engine flare
+    [SerializeField] GameObject engine_flare;
 
     private float main_engine_output;
     private float flfp_engine_output;
@@ -43,6 +45,14 @@ public class BikeControl : MonoBehaviour
         {
             gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * main_engine_output);
         }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            engine_flare.active = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            engine_flare.active = false;
+        }
         if (Input.GetKey(KeyCode.S))
         {
         }
@@ -56,13 +66,13 @@ public class BikeControl : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.GetComponent<Rigidbody>().AddForceAtPosition(8f * transform.right, transform.position + 2 * transform.forward - 0.1f * transform.up);
+            gameObject.GetComponent<Rigidbody>().AddForceAtPosition(8f * transform.right, transform.position + 2 * transform.forward - 0.2f * transform.up);
             //gameObject.GetComponent<Rigidbody>().AddForce(0.05f * transform.right, ForceMode.Impulse);
 
         }
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.GetComponent<Rigidbody>().AddForceAtPosition(-8f * transform.right, transform.position + 2 * transform.forward - 0.1f * transform.up);
+            gameObject.GetComponent<Rigidbody>().AddForceAtPosition(-8f * transform.right, transform.position + 2 * transform.forward - 0.2f * transform.up);
             //gameObject.GetComponent<Rigidbody>().AddForce(-0.05f * transform.right, ForceMode.Impulse);
 
         }
@@ -74,27 +84,18 @@ public class BikeControl : MonoBehaviour
         // gameObject.GetComponent<Rigidbody>().AddForceAtPosition(new Vector3(0, 9.81f + 20 * delta_height, 0), transform.position + transform.up * 1f - transform.forward * 2f);
 
         // front left engine
-        flfp_engine_output = 4f + 5f * (preset_flight_height - flfp.transform.position.y + 0.8f);
-        if (flfp_engine_output < 0)
-        {
-            flfp_engine_output = 0;
-        }
+        flfp_engine_output = 15f + 5f * (preset_flight_height - flfp.transform.position.y);
+
         gameObject.GetComponent<Rigidbody>().AddForceAtPosition(-flfp.transform.right * flfp_engine_output, flfp.transform.position);
 
         // front right engine
-        frfp_engine_output = 4f + 5f * (preset_flight_height - frfp.transform.position.y + 0.8f);
-        if (frfp_engine_output < 0)
-        {
-            frfp_engine_output = 0;
-        }
+        frfp_engine_output = 15f + 5f * (preset_flight_height - frfp.transform.position.y);
+
         gameObject.GetComponent<Rigidbody>().AddForceAtPosition(-frfp.transform.right * frfp_engine_output, frfp.transform.position);
-        Debug.Log(frfp.transform.position);
+        Debug.Log(frfp_engine_output);
         // back engine
-        bfp_engine_output = 9.8f + 5 * (preset_flight_height - bfp.transform.position.y - 2f);
-        if (bfp_engine_output < 0)
-        {
-            bfp_engine_output = 0;
-        }
+        bfp_engine_output = 10f + 2.5f * (preset_flight_height - bfp.transform.position.y);
+
         gameObject.GetComponent<Rigidbody>().AddForceAtPosition(new Vector3(0, 10, 0) * bfp_engine_output, bfp.transform.position);
     }
 }
